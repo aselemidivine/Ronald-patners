a2ffee23d73fa6e6f4740b4ac25b5fbf4057f56c
Aselemi Divine <aselemidivine@gmail.com>
2023-06-27 17:07:52 +0100

    added more pictures

--- assets/js/script.js
+++ assets/js/script.js
@@ -180,6 +180,29 @@ window.addEventListener("scroll", () => {
     }
 })
 
+// SWIPPER JS
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
 
 // POPUP AFTER FORM SUBMISSION
 // let popup = document.getElementById("popup");
