d5ae0b6f7c273c434ac09f0078647f61139ed9af
Abasir04 <adiadebayo1@gmail.com>
2026-03-02 14:42:57 +0100

    api update

--- assets/js/eventsForm.js
+++ assets/js/eventsForm.js
@@ -1,29 +1,3 @@
-const formVValue = document.querySelector('.formm');
-
-//reset form value...
-const resetForm = () => {
-  formVValue.reset();
-};
-
-formVValue.addEventListener('submit', function(e) {
-  e.preventDefault();
-  //send request using urlencoded
-  const prePayload = new FormData(formVValue);
-  const payload = new URLSearchParams(prePayload);
-  console.log([...payload]);
-
-  //plug into the database .....
-  // replace url with your api url
-    fetch('https://api.ronald-partners.net/api/events-form', {
-    // fetch('http://localhost:3500/api/events-form', {
-    method: 'POST',
-    body: payload,
-     mode: 'no-cors'
-  })
-
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-
-  resetForm();
-});
+// Form submission is now handled universally by sweetalert.js
+// This file is kept for backward compatibility but is intentionally empty.
+// All forms are automatically handled by the universal handler in sweetalert.js.
